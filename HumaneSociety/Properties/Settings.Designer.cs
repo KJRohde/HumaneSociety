7fcf4d63b662685381595df754578d4d5506ac46
MrMencia <Avimencia@gmail.com>
2019-08-27 11:30:18 -0800

    pulling info

--- HumaneSociety/Properties/Settings.Designer.cs
+++ HumaneSociety/Properties/Settings.Designer.cs
@@ -33,5 +33,15 @@ namespace HumaneSociety.Properties {
                 return ((string)(this["HumaneSocietyConnectionString3"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("y")]
+        public string hngmh {
+            get {
+                return ((string)(this["hngmh"]));
+            }
+        }
     }
 }
